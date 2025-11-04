ecbb04a62a0f530e447befb5a71e7083fba89059
Yuri Levi Estevam Santos <120196956+YuriLeviEstevam@users.noreply.github.com>
2025-11-04 20:22:12 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -132,7 +132,7 @@ function page_form(string $flash = '', array $old = []): string
     $cell = h($old['cell'] ?? '');
     $email = h($old['email'] ?? '');
 
-    return <HTML;
+    return <<<HTML;
 <!doctype html>
 <html lang="pt-br">
 <head>
