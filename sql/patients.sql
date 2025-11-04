c2762956bb2a390a21edd02f3ba64a8d64b1bd1f
Lucas Coque Ulian <luc.coq.dev@gmail.com>
2025-11-04 19:33:12 -0300

    Update patients.sql

--- sql/patients.sql
+++ sql/patients.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS patients (
   id BIGSERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL CHECK (name ~ '^[A-Za-zÀ-ÿ\s]+$'),
+  name VARCHAR(100) NOT NULL CHECK (name ~ '^[A-Za-zÀ-ÿ\s]$'),
   cpf CHAR(11) UNIQUE CHECK (cpf ~ '^[0-9]{11}$'),
   birth_date DATE CHECK (birth_date <= CURRENT_DATE),
   phone VARCHAR(15) CHECK (phone ~ '^[0-9]{8,15}$'),
